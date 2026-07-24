ad8d6795f9f3a303ae6e49774e95f668b3a828b3
unknown <priya@gmail.com>
2026-07-24 15:23:14 +0530

    Implemented Logout Component

--- src/components/LogoutComp.jsx
+++ src/components/LogoutComp.jsx
@@ -1,3 +1,52 @@
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { logout } from "../redux/authSlice";
+// import { useNavigate } from "react-router-dom";
+
+// export default function LogoutComp() {
+
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+
+//     dispatch(logout());
+
+//     localStorage.removeItem("token");
+//     localStorage.removeItem("user");
+
+//     setTimeout(() => {
+//       navigate("/");
+//     }, 1000);
+
+//   }, [dispatch, navigate]);
+
+//   return (
+//     <div
+//       className="d-flex justify-content-center align-items-center"
+//       style={{ height: "100vh" }}
+//     >
+
+//       <div className="text-center">
+
+//         <div
+//           className="spinner-border text-primary mb-3"
+//           role="status"
+//         >
+//         </div>
+
+//         <h3>Logging Out...</h3>
+
+//         <p className="text-muted">
+//           Redirecting to Home Page
+//         </p>
+
+//       </div>
+
+//     </div>
+//   );
+// }
+
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../redux/authSlice";
@@ -10,13 +59,15 @@ export default function LogoutComp() {
 
   useEffect(() => {
 
+    // Clear Redux State
     dispatch(logout());
 
-    localStorage.removeItem("token");
+    // Clear Local Storage
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
 
     setTimeout(() => {
-      navigate("/");
+      navigate("/login");
     }, 1000);
 
   }, [dispatch, navigate]);
@@ -26,23 +77,21 @@ export default function LogoutComp() {
       className="d-flex justify-content-center align-items-center"
       style={{ height: "100vh" }}
     >
-
       <div className="text-center">
 
         <div
           className="spinner-border text-primary mb-3"
           role="status"
-        >
-        </div>
+        ></div>
 
         <h3>Logging Out...</h3>
 
         <p className="text-muted">
-          Redirecting to Home Page
+          Redirecting to Login Page
         </p>
 
       </div>
-
     </div>
   );
 }
+
