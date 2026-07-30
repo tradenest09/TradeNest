9d3e8a1a53e55bd0d50cd9fc08badf1ce1990800
eshan <eshanyadav32@gmail.com>
2026-07-30 13:47:25 +0530

    feat(order-service): secure REST APIs using JWT authentication

---
+++ BackendSB/order-service/src/main/java/com/tradenest/orderservice/security/JwtService.java
@@ -0,0 +1,60 @@
+package com.tradenest.orderservice.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+
+	public String generateToken(String email) {
+
+		return Jwts.builder().setSubject(email).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+				.signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	public String extractUsername(String token) {
+
+		return extractAllClaims(token).getSubject();
+	}
+
+	public boolean isTokenValid(String token, String email) {
+
+		String username = extractUsername(token);
+
+		return username.equals(email) && !isTokenExpired(token);
+	}
+
+	private boolean isTokenExpired(String token) {
+
+		return extractAllClaims(token).getExpiration().before(new Date());
+	}
+
+	private Claims extractAllClaims(String token) {
+
+		return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+	}
+
+	private Key getSignInKey() {
+
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+}
